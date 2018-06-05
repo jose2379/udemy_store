740221d6877f648574437a7371d0e92fea409618
Jose María Miñambres Redondo <josemariaminambresredondo@iMac-de-Jose.local>
2018-06-06 00:05:46 +0200

    S4c10

--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -1,12 +1,19 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import {  MatToolbarModule
+           } from '@angular/material';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 
 @NgModule({
   imports: [
     CommonModule
+
   ],
   declarations: [],
   exports: [
+    FormsModule,
+    ReactiveFormsModule,
+    MatToolbarModule
     // MatCardModule
   ]
 })
